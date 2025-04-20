a9d02d689cee0860a1d9d0fae16924515e91d008
madhanraj <madhanraj10799@gmail.com>
2025-04-20 21:56:36 +0530

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oqua4gthgsg"
+    "revision": "0.8nr6mcehap"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
